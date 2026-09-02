74657f4ce82644d71b14b664c0a1b9445c9046f9
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-09-02 08:37:06 -0300

    Criando um package com a classe principal e classe de regras

---
+++ src/Aula_02/Main.java
@@ -0,0 +1,14 @@
+package Aula_02;
+
+// Revisão de java - INICIO
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome");
+
+        for (int i = 1; i <= 5; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
+
---
+++ src/Aula_02/revisao.java
@@ -0,0 +1,6 @@
+package Aula_02;
+
+// Revisão de java - INICIO
+
+public class revisao {
+}
